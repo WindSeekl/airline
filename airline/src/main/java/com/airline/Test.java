93b5c2c70b10352d92834f24ae8cdf677b2124f9
WangYu <1074067438@qq.com>
2020-04-13 11:27:19 +0800

    老王的上传

---
+++ airline/src/main/java/com/airline/Test.java
@@ -0,0 +1,13 @@
+package com.airline;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("一袋米要扛几楼");
+		System.out.println("一袋米要扛二楼");
+		System.out.println("啥是gay？那撸多！");
+		System.out.println("辛辣天塞");
+	}
+
+}
