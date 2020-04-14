99eef8a77f64fe531eca04e836624e476291649e
ligang6-22 <1289996240@qq.com>
2020-04-14 15:40:15 +0800

    qq

--- airline/src/main/java/com/airline/repository/ReserveRepository.java
+++ airline/src/main/java/com/airline/repository/ReserveRepository.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 import com.airline.domain.Reserve;
 @Mapper
 public interface ReserveRepository {
-	List<String> queryReserve(Reserve reserve);//查询订单(用户名/订单号/订票日期)
+	List<Reserve> queryReserve(Reserve reserve);//查询订单(用户名/订单号/订票日期)
 	int insertReserve(Reserve reserve);//下单
 	int updateState(Reserve reserve);//订单状态
 }
--- airline/src/main/java/com/airline/service/ReserveService.java
+++ airline/src/main/java/com/airline/service/ReserveService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.airline.domain.Reserve;
 
 public interface ReserveService {
-	List<String> queryReserve(Reserve reserve);//查询订单(用户名/订单号/订票日期)
+	List<Reserve> queryReserve(Reserve reserve);//查询订单(用户名/订单号/订票日期)
 	int insertReserve(Reserve reserve);//下单
 	int updateState(Reserve reserve);//订单状态
 }
